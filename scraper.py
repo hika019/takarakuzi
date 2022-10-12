fc0f8e18815c7cc92181d8234ae70782507d12c2
hika019 <ikazuti2001@gmail.com>
2022-10-13 05:09:41 +0900

    webからのデータ取得に正規表現を使用

--- scraper.py
+++ scraper.py
@@ -1,82 +1,67 @@
 # -*- coding: utf-8 -*-
-import numpy as np
 import requests
 import csv
-import sympy
+import re
 import pandas as pd
 from bs4 import BeautifulSoup
 
 
 
+
 def scraper(start):
     
-    if start < 1000:
-        start = "0"+str(start)
-    else:
-        start = str(start)
-
+    start = '0'*4+str(start)
+    start = start[-4:]
+    
     url = "https://takarakuji-loto.jp/loto6-mini/loto6"+(start)+".html"
+    print(start)
+    #url = "https://takarakuji-loto.jp/loto6-mini/loto60001.html"
 
     html = requests.get(url)
     soup = BeautifulSoup(html.content, "html.parser")
 
     body = soup.find("section")
 
-    all_table = soup.select("table", limit=None)
-    datas=[]
+    tables = soup.select("table", limit=None)
 
 
-    for index in range(1, len(all_table)):
-        lists = all_table[index].select("td", limit=None)
-        
-        #print(len(lists)/20)
-        for i in range(int(len(lists)/20)):
-            data = str(lists[2+i*21-i:9+i*21-i]).replace('<td class="bg1">\r\n\t\t\t\t', "")
-            data = data.replace("\t\t\t</td>", "")
-            data = data.replace('<td class="bg1 bwaku">', "")
-            data = data.replace('<td class="bg1">', "")
-            data = data.replace('</td>', "")
-            data = data.replace('<td bgcolor="#f0f1fc" class="bg1">\r\n\t\t\t\t', "")
-            data = data.replace('<td class="bg1" width="21">', '')
-            data = data.replace('<td class="bg1" width="19">', '')
-            data = data.replace('<td bgcolor="#f0f1fc" class="bg1">', '')
-            data = data.replace('<td bgcolor="#f0f1fc" class="bg1 bwaku">', '')
-            data = data.replace('<br/>', '')
-            
-            #print(data)
-            
-            if data[1:3] == ", ":
-                break
-            #print(data[1:3])
-            
-            data_list = []
-            data_list.append(int(data[1:3]))
-            data_list.append(int(data[5:7]))
-            data_list.append(int(data[9:11]))
-            data_list.append(int(data[13:15]))
-            data_list.append(int(data[17:19]))
-            data_list.append(int(data[21:23]))
-            data_list.append(int(data[-4:-1]))
-            
-            datas.append(data_list)
-        
+    nums = []
+
+    for i in range(len(tables)):
+        table = str(tables[i]).split('</tr>\n')
+        for j in range(len(table)):
+            if "第" in table[j]:
+                table[j] = table[j][:table[j].find('style="text-align:right;">')]
+                
+                table[j] = re.sub('(第\d+回)|(ロト6)|((\w|\s)+年)|((\w|\s)+日)|(".*")', '', table[j])
+                table[j] = re.sub('\D', '', table[j])
+                
+                if len(table[j]) != 14:
+                    break
+                
+                table[j] = re.split('(..)', table[j])[1::2]
+                table[j] = list(map(int, table[j]))
+                nums.append(table[j])
     
-            print((data_list))
+    return nums
 
-    with open("6-"+str(start)+"-"+str(int(start)+49)+".csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(datas)
-    
-    with open("6-all.csv", "a", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(datas)
 
 def create_csv():
     with open("6-all.csv", "w", newline="") as f:
             writer = csv.writer(f)
 
-    for start in range(1, 1611, 50):
-        scraper(start)
+    for start in range(1, 1651, 50):
+        data = scraper(start)
+        print(len(data))
+        with open("6-"+str(start)+"-"+str(int(start)+49)+".csv", "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
+    
+    
+    with open("6-all.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+        
 
 def read_data():
     #create_csv()
@@ -84,17 +69,7 @@ def read_data():
     datas = df.values
     return datas
 
-
-
-
-
-
-
-
-
-
-
-
+        
 
 
 
