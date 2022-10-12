1b440a8d5edaceeb2acdbe2891c2c6a3ba4b99a8
hika019 <ikazuti2001@gmail.com>
2022-10-13 05:21:52 +0900

    第851回以降のデータが読み取れないバグを修正

--- scraper.py
+++ scraper.py
@@ -28,7 +28,7 @@ def scraper(start):
     nums = []
 
     for i in range(len(tables)):
-        table = str(tables[i]).split('</tr>\n')
+        table = str(tables[i]).split('</tr>')
         for j in range(len(table)):
             if "第" in table[j]:
                 table[j] = table[j][:table[j].find('style="text-align:right;">')]
@@ -50,7 +50,7 @@ def create_csv():
     with open("6-all.csv", "w", newline="") as f:
             writer = csv.writer(f)
 
-    for start in range(1, 1651, 50):
+    for start in range(1, 1652, 50):
         data = scraper(start)
         print(len(data))
         with open("6-"+str(start)+"-"+str(int(start)+49)+".csv", "w", newline="") as f:
@@ -58,9 +58,9 @@ def create_csv():
             writer.writerows(data)
     
     
-    with open("6-all.csv", "a", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+        with open("6-all.csv", "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
         
 
 def read_data():
@@ -71,9 +71,40 @@ def read_data():
 
         
 
+start = 1651
+
+start = '0'*4+str(start)
+start = start[-4:]
+
+url = "https://takarakuji-loto.jp/loto6-mini/loto6"+(start)+".html"
+print(start)
+#url = "https://takarakuji-loto.jp/loto6-mini/loto60001.html"
+
+html = requests.get(url)
+soup = BeautifulSoup(html.content, "html.parser")
+
+body = soup.find("section")
+
+tables = soup.select("table", limit=None)
 
 
+nums = []
 
+for i in range(len(tables)):
+    table = str(tables[i]).split('</tr>')
+    for j in range(len(table)):
+        if "第" in table[j]:
+            table[j] = table[j][:table[j].find('style="text-align:right;">')]
+            
+            table[j] = re.sub('(第\d+回)|(ロト6)|((\w|\s)+年)|((\w|\s)+日)|(".*")', '', table[j])
+            table[j] = re.sub('\D', '', table[j])
+            
+            if len(table[j]) != 14:
+                break
+            
+            table[j] = re.split('(..)', table[j])[1::2]
+            table[j] = list(map(int, table[j]))
+            nums.append(table[j])
 
 
 
